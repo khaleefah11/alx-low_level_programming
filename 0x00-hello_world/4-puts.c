28864922a4df3db4b18b0cf2107d2b83b969b68d
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-07 09:39:34 -0700

    C file using puts to output a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+	/* initializing str */
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	// writing to stdout
+	puts(str);
+
+	return 0;
+}
