980e4f6c101c6ad178ece9de3972d74ed8f327dc
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-07 10:43:37 -0700

    C file to print an output using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 0
+ */
+int main(void)
+{
+
+	/* writing to stdout */
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+
+	return (0);
+}
