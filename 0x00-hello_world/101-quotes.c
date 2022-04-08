18d2a18aec80ffe30d5b9a73b32ff1afea45317c
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-08 04:45:19 -0700

    Program to output to stderr - quotes.c

---
+++ 0x00-hello_world/101-quotes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 1
+ */
+int main(void)
+{
+
+	/* writing to stderr */
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
