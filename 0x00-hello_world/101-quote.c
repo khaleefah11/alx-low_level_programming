37ee676f2dbb833c5bd6bb621840ec0eb9ca1067
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-08 07:33:50 -0700

    Modify the code in quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 {
 
 	/* initializing and declaring strings */
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	/* writing to stderr */
 	fprintf(stderr, "%s", str1);
