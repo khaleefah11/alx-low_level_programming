b036185aa0ed9870b5214696d2f0f064a12ff2a2
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-08 06:05:46 -0700

    Modify the code in quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,10 @@
 int main(void)
 {
 
-	/* initializing strings */
+	/* initializing and declaring strings */
 	char str1[] = "and that piece of art is useful\"";
 	char str2[] = " - Dora Korpar, 2015-10-19";
+	char str3[];
 
 	/* concatenating strings */
 	char str3[] = strcat(str1, str2);
