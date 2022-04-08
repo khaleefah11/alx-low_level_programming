d1e472d13cc9bff4aac38148da213d21ecb1fe5f
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-08 03:51:32 -0700

    Modify the code to in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,19 +9,12 @@
 int main(void)
 {
 
-	/* initializing the variables */
-	char charType;
-	int intType;
-	long int longInt;
-	long long int longLongInt;
-	float floatType;
-
 	/* sizeof evaluates the sizes of variables */
-	printf("Size of a char: %zu byte(s)\n" sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n" sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n" sizeof(longInt));
-	printf("Size of a long long int: %zu byte(s)\n" sizeof(longLongInt));
-	printf("Size of a float: %zu byte(s)\n" sizeof(floatType));
+	printf("Size of a char: %d byte(s)\n", sizeof(char);
+	printf("Size of an int: %d byte(s)\n", sizeof(int);
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int);
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int);
+	printf("Size of a float: %d byte(s)\n", sizeof(float);
 
 	return (0);
 }
