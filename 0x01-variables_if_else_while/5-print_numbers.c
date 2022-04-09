d31e74ef49afd72e64c9b20636cc32b72061628a
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-09 05:40:36 -0700

    Program to print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 0 upon success
+ */
+int main(void)
+{
+
+	int digit;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	printf('\n');
+
+
+	return (0);
+}
