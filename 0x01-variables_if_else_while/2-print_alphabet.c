023c40920f3f16c5b11a0de04b59a8190cf18f9f
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-09 04:52:26 -0700

    Program to print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 0 upon success
+ */
+int main(void)
+{
+
+	char alphabet;
+
+	for (alphabet = a; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
