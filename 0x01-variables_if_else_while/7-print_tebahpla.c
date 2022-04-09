24f08b6223ac1fe6708938e4f9ac279092298d95
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-09 06:18:20 -0700

    Program to print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 0 upon success
+ */
+int main(void)
+{
+
+	char alphabet;
+
+	for (alphabet = 122; alphabet >= '97'; alphabe--)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
