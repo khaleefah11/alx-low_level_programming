770cef744fca6b5767ccf2bf238111c8904652ae
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-09 05:09:19 -0700

    Program to alternate printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 0 upon success
+ */
+int main(void)
+{
+
+	char upper_case;
+	char lower_case;
+
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
+	}
+
+	for (lower_case = 'a'; lower_case <= 'a'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
