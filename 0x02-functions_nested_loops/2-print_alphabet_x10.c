a9657178cd1b3488dbca039879d4956ced35e2b7
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-12 16:32:01 -0700

    Code to print lowercase alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print alphabets in lowercase 10x
+ * Void: Returs no parameters
+ *
+ * Return: Return the value of 0 upon success
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alpha;
+
+	while (++count <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; ++alpha)
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
