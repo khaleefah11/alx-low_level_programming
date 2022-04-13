5d27479e0ba6b4e8513e43ba6a781af8f2b39014
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-12 18:50:50 -0700

    Code to print the times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - Function to print the times table
+ *
+ */
+void times_table(void)
+{
+	int num, mult, result;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		_putchar('0');
+
+		for  (mult = 1; mult <= 9; ++mult)
+		{
+
+			_putchar(',');
+			_putchar(' ');
+
+			result = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
