875a3f3b45f3f2ee4b53890e3cfd37f03a314919
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-12 18:01:58 -0700

    Code to calculate the absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - function to calculate the absolute value of an int
+ *@n: The int type parameter the function takes
+ *
+ * Return: Return the computed absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (-n);
+}
