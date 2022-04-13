761c3c55699452f1d6b5ee9b6049c36fce7a028d
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-12 17:00:56 -0700

    Code to check whether or not an alphabet is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * islower - function to whether or not an alphabets is lowercase
+ * Takes an int type parameter
+ *
+ * Return: Return the value of 0 upon success
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);	
+}
