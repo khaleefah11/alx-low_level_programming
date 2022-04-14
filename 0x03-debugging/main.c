5118b36e4bac91f30ec3d09d7065449f9dd2d95e
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-14 16:24:23 -0700

    Program to test if the positive_or_negative function works as expected

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that checks whether an integer is +ve or -ve
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i  = 0;
+
+	positive-or_negative(i);
+
+	return (0);
+}
