838a6c669eb1445212e9fa0767c98b2023c5070c
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-14 14:23:34 -0700

    Program to print numbers less than 10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Function to print numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar("i");
+	}
+
+	_putchar("\n");
+}
