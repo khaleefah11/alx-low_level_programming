3e710e4e26d9382c39ade773a86e3114576da71b
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-15 14:01:50 -0700

    Program to print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: length of the line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
