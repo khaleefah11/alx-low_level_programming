5763e1792479b7356842c341f80c0c75bc9973ea
khaleefah11 <raheemibnsalam@gmail.com>
2022-04-25 12:28:52 -0700

    Create readme, main.h: Function to concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+ * *_strcat - Function that concatenates two strings
+ * @dest: second string to be concatenated
+ * @src: first string to be appended to dest
+ * Return: Returns a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+	{
+		for (y = 0; src[y] != '\0'; y++)
+		{
+			dest[x] = src[y];
+			x++;
+		}
+
+		dest[x] = '\0';
+
+		return (dest);
+	}
+}
