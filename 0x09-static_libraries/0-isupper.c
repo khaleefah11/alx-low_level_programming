426578c834293e3ba7381603a48811d5edc6c50e
khaleefah11 <raheemibnsalam@gmail.com>
2022-05-07 14:14:00 -0700

    Static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks whether a char is in uppercase
+ * @c: The parameter which the function acts upon
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
