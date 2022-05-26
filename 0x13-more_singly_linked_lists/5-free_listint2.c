6eb135931b8d1fe3f1e354dad25a86e2274ce1c6
khaleefah11 <raheemibnsalam@gmail.com>
2022-05-26 12:59:33 -0700

    Program that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that frees a listint_t list
+ * @head: head of a list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+
+		*head = NULL;
+	}
+}
